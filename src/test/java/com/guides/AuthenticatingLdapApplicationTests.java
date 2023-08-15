36dd801ecca9e0d811f55bf403a194613be60028
simsonmoses <sims91502@gmail.com>
2023-08-15 14:12:43 +0530

    init

---
+++ src/test/java/com/guides/AuthenticatingLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guides;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticatingLdapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
